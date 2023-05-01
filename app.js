9f2f3549d794ad9becd4585534120f13c03a4cc0
Bliss  Armah-Nwanwah <bliss.armah-nwanwah@amalitech.com>
2023-05-01 14:44:25 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const user = require('./routes/user')
+const connectDB = require('./db/connect')
+
+
+const app = express()
+
+
+
+require('dotenv').config()
+// middleware
+app.use(express.json())
+
+
+
+// routes
+app.get('/hello',(req,res)=>{
+  res.send('user auth')
+})
+
+app.use('/api/v1/user',user)
+
+
+const PORT = process.env.PORT || 4000;
+
+const start = async() =>{
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(PORT,console.log(`server is listening on port ${PORT}...`))
+  } catch (error) {
+   console.log(error); 
+  }
+}
+
+start()
---
+++ controllers/user.js
@@ -0,0 +1,99 @@
+const User = require("../models/User");
+const {  StatusCodes } = require("http-status-codes");
+const { BadRequestError } = require("../errors");
+
+const registerUser = async (req, res) => {
+  const {email} = req.body
+  try {
+    const existingUser = await User.findOne( {email} )
+    if (existingUser) {
+      throw new BadRequestError('Email Already Exist');
+    }
+  const user = await User.create({ ...req.body });
+  const token = await user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user:{firstName: user.firstName,lastName: user.lastName, email: user.email,phoneNumber: user.phoneNumber,gender: user.gender, image: user.image} ,token});
+} catch (error) {
+  res.status(error.statusCode || 500).json({ error: error.message });
+}
+};
+ 
+const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid email or password" });
+    }
+    const isMatch = await user.comparePassword(req.body.password);
+    if (!isMatch) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid email or password" });
+    }
+    const token = user.createJWT();
+    return res.status(StatusCodes.OK).json({ 
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        gender: user.gender,
+        image: user.image
+      }, 
+      token
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Something went wrong" });
+  }
+};
+
+
+
+const updateUser = async (req, res) => {
+  const {firstName,lastName,email,phoneNumber,image,gender} = req.body
+  if (!firstName ||  !lastName || !email || !phoneNumber  || !image || gender) {
+    res.status(StatusCodes.BAD_REQUEST).json({msg:'Please provide all values'})
+  }
+  const user = await User.findOne({_id : req.user.userId})
+  user.email = email
+  user.firstName = firstName
+  user.lastName = lastName
+  user.phoneNumber = phoneNumber
+  user.gender = gender
+  user.image = image
+
+  await user.save()
+  const token = user.createJWT()
+  res
+  .status(StatusCodes.OK)
+  .json({
+    user: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      gender: user.gender,
+      image: user.image,
+    },
+    token
+  });
+};
+
+const changePassword = async (req, res) => {
+  console.log(req.user);
+  try {
+    const user = await User.findById(req.user.userId);
+    const isMatch = await user.comparePassword(req.body.oldPassword);
+    if (!user) {
+      return res.status(404).json({ message: "User does not exist" });
+    }
+    if (!isMatch) {
+      return res.status(404).json({ message: "Password doesn't match the old password" });
+    }
+    user.password = req.body.password;
+    await user.save();
+    res.json({ message: "Password changed successfully" });
+  } catch (error) {
+    res.status(400).json({ error:"hi" });
+  }
+};
+
+module.exports = { registerUser, loginUser, updateUser, changePassword };
